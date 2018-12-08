376fcd8391848ad9d61a71a06e39188f2b70c4b1
Max <max.gabrielsson@zenuity.com>
2018-12-08 11:19:23 +0100

    Day8

--- src/main/java/com/gabrielsson/adventofcode/Day8.java
+++ src/main/java/com/gabrielsson/adventofcode/Day8.java
@@ -2,63 +2,57 @@ package com.gabrielsson.adventofcode;//package com.gabrielsson.adventofcode;
 
 import java.util.*;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day8 {
     List<Integer> rows;
-    int index = 0;
+    int index;
 
     public Object part1(List<Integer> rows) {
         this.rows = rows;
-        return getChildNode().metaSum;
+        index = 0;
+        return getChildNode().getSumPart1();
     }
 
     public Object part2(List<Integer> rows) {
         this.rows = rows;
-
-        Node node = getChildNode();
-        return node.getMetaSum();
+        index = 0;
+        return getChildNode().getSumPart2();
     }
-
 
     private Node getChildNode() {
         Node node = new Node();
-
         node.numberOfChildNodes = this.rows.get(index++);
         node.numberOfMetaEntries = this.rows.get(index++);
 
-        for (int i = 0; i < node.numberOfChildNodes; i++) {
-            Node child = getChildNode();
-            node.children.add(child);
-            node.metaSum += child.metaSum;
-        }
-
-
-        for (int i = 0; i < node.numberOfMetaEntries; i++) {
-            node.metaentries.add(this.rows.get(index));
-            node.metaSum += this.rows.get(index++);
-        }
+        IntStream.range(0, node.numberOfChildNodes).forEach((i) ->  node.children.add(getChildNode()));
+        IntStream.range(0,node.numberOfMetaEntries).forEach((i) -> node.metaEntries.add(this.rows.get(index++)));
         return node;
     }
 
     static class Node {
-        int metaSum = 0;
         int numberOfChildNodes;
         int numberOfMetaEntries;
-        List<Integer> metaentries = new ArrayList<>();
+        List<Integer> metaEntries = new ArrayList<>();
         List<Node> children = new ArrayList<>();
 
-        public int getMetaSum() {
+        public int getSumPart1() {
+            return children.stream()
+                    .mapToInt(Node::getSumPart1)
+                    .sum() +
+                    metaEntries.stream()
+                            .mapToInt(Integer::new)
+                            .sum();
+        }
+
+        public int getSumPart2() {
             if (numberOfChildNodes > 0) {
-                return metaentries.stream()
+                return metaEntries.stream()
                         .filter(metaIndex -> metaIndex - 1 < children.size())
-                        .mapToInt(metaIndex -> children.get(metaIndex - 1).getMetaSum())
+                        .mapToInt(metaIndex -> children.get(metaIndex - 1).getSumPart2())
                         .sum();
-
-
-
             } else {
-                return metaentries.stream()
+                return metaEntries.stream()
                         .mapToInt(Integer::new)
                         .sum();
             }
